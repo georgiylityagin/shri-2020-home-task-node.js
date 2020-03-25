7eb22ec9ebb59fb6dc804112a1da381db3281d34
georgiylityagin <geolit94@gmail.com>
2020-03-25 13:51:04 +0300

    add simple routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
+import { StartPage } from './components/StartPage/StartPage';
+import { HistoryPage } from './components/HistoryPage/HistoryPage';
+import { SettingsPage } from './components/SettingsPage/SettingsPage';
+import { DetailsPage } from './components/DetailsPage/DetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello, world
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact><StartPage /></Route>
+        <Route path='/settings'><SettingsPage /></Route>
+        <Route path='/history'><HistoryPage /></Route>
+      </Switch>
+    </Router>
   );
 }
 
