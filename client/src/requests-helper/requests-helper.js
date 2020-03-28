05cae389375c3202c377c41d394bc62acbccf578
georgiylityagin <geolit94@gmail.com>
2020-03-28 20:16:26 +0300

    create helper for requests to the server

---
+++ client/src/requests-helper/requests-helper.js
@@ -0,0 +1,61 @@
+export const api = {
+  getConfig() {
+    return fetch('http://127.0.0.1:3000/api/settings')
+      .then(res => res.json())
+      .then(resBody => resBody)
+      .catch(err => console.error(err));
+  },
+
+  getBuildsList(offset = 0, limit = 25) {
+    return fetch(`http://127.0.0.1:3000/api/builds?offset=${offset}&limit=${limit}`)
+      .then(res => res.json())
+      .then(resBody => resBody)
+      .catch(err => console.error(err))
+  },
+
+  getBuildDetails(buildId) {
+    return fetch(`http://127.0.0.1:3000/api/builds/${buildId}`)
+      .then(res => res.json())
+      .then(resBody => resBody)
+      .catch(err => console.error(err))
+  },
+
+  getBuildLogs(buildId) {
+    return fetch(`http://127.0.0.1:3000/api/builds/${buildId}/logs`)
+      .then(res => res.json())
+      .then(resBody => resBody)
+      .catch(err => console.error(err))
+  },
+
+  postConfig(config) {
+    const bodyData = JSON.stringify(config);
+
+    return fetch('http://127.0.0.1:3000/api/settings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': bodyData.length.toString()
+      },
+      body: bodyData
+    })
+    .then(res => res.json())
+    .then(resBody => resBody)
+    .catch(err => console.error(err))
+  },
+
+  postAddBuild(buildData) {
+    const bodyData = JSON.stringify(buildData);
+
+    return fetch(`http://127.0.0.1:3000/api/builds/${buildData.commitHash}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': bodyData.length.toString()
+      },
+      body: bodyData
+    })
+    .then(res => res.json())
+    .then(resBody => resBody)
+    .catch(err => console.error(err))
+  }
+}
