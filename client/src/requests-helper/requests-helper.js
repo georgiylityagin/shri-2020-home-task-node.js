96e0aa08cebb6f4c44e328e8af9dc3d116068f19
georgiylityagin <geolit94@gmail.com>
2020-04-03 00:10:52 +0300

    refactoring api

--- client/src/requests-helper/requests-helper.js
+++ client/src/requests-helper/requests-helper.js
@@ -1,61 +1,55 @@
+const fetchData = async (url, options = {}) => {
+  const baseUrl = 'http://127.0.0.1:3000/api/';
+
+  const timeout = new Promise(function(resolve, reject) { 
+    setTimeout(reject, 5000, new Error('Сервер долго не отвечает'));
+  });
+
+  try {
+    const response = await Promise.race([fetch(baseUrl + url, options), timeout]);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const api = {
   getConfig() {
-    return fetch('http://127.0.0.1:3000/api/settings')
-      .then((res) => res.json())
-      .then((resBody) => resBody)
-      .catch((err) => console.error(err));
+    return fetchData('settings');
   },
 
-  getBuildsList(offset = 0, limit = 25) {
-    return fetch(
-      `http://127.0.0.1:3000/api/builds?offset=${offset}&limit=${limit}`
-    )
-      .then((res) => res.json())
-      .then((resBody) => resBody)
-      .catch((err) => console.error(err));
+  getBuildsList(offset = 0, limit = 100) {
+    return fetchData(`builds?offset=${offset}&limit=${limit}`);
   },
 
   getBuildDetails(buildId) {
-    return fetch(`http://127.0.0.1:3000/api/builds/${buildId}`)
-      .then((res) => res.json())
-      .then((resBody) => resBody)
-      .catch((err) => console.error(err));
+    return fetchData(`builds/${buildId}`);
   },
 
   getBuildLogs(buildId) {
-    return fetch(`http://127.0.0.1:3000/api/builds/${buildId}/logs`)
-      .then((res) => res.json())
-      .then((resBody) => resBody)
-      .catch((err) => console.error(err));
+    return fetchData(`builds/${buildId}/logs`);
   },
 
   postConfig(config) {
     const bodyData = JSON.stringify(config);
 
-    return fetch('http://127.0.0.1:3000/api/settings', {
+    return fetchData('settings', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Content-Length': bodyData.length.toString(),
       },
       body: bodyData,
-    })
-      .then((res) => res.json())
-      .then((resBody) => resBody)
-      .catch((err) => console.error(err));
+    });
   },
 
   postAddBuild(commitHash) {
-    return fetch(`http://127.0.0.1:3000/api/builds/${commitHash}`, {
+    return fetchData(`builds/${commitHash}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Content-Length': commitHash.length.toString(),
       },
-      // body: bodyData
-    })
-      .then((res) => res.json())
-      .then((resBody) => resBody)
-      .catch((err) => console.error(err));
-  },
-};
+    });
+  }
+}
