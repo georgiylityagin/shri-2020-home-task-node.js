f80fb571d947449337f949a41f855bbfdef0cb4c
georgiylityagin <geolit94@gmail.com>
2020-04-14 01:00:02 +0300

    delete comments

--- tests/.hermione.conf.js
+++ tests/.hermione.conf.js
@@ -1,8 +1,6 @@
 module.exports = {
   baseUrl: 'http://127.0.0.1:3005/',
   gridUrl: 'http://0.0.0.0:4444/wd/hub',
-  // screenshotMode: 'fullpage',
-  // screenshotsDir: 'hermione/screens',
 
   retry: 3,
 
